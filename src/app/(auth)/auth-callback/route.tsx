a6448402b3517de42587069eda78065535a19216
CHARAN T M <charanntm.dev@gmail.com>
2026-07-25 14:41:45 +0530

    updated the exchage loggin

--- src/app/(auth)/auth-callback/route.tsx
+++ src/app/(auth)/auth-callback/route.tsx
@@ -3,17 +3,13 @@ import { createClient } from '../../../lib/supabase/server';
 import { NextResponse } from 'next/server';
 import { createUserProfile, getUserProfile, getUserProfileWithOnboarding } from '../../../lib/queries/user';
 import { UserInsert, UserRow } from '../../../types/supabase';
+import { error } from 'node:console';
 
 export async function GET(request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get('code');
   const type = searchParams.get('type');
 
-  console.log({
-    url: request.url,
-    code: searchParams.get('code'),
-    type: searchParams.get('type'),
-  });
 
   if (!code) {
     return NextResponse.redirect(`${origin}/login?error=auth_callback_failed`);
@@ -21,6 +17,8 @@ export async function GET(request) {
 
   const supabase = await createClient();
   const { error: exchangeError } = await supabase.auth.exchangeCodeForSession(code);
+
+  console.log("Exchange Error", error)
 
   if (exchangeError) {
     return NextResponse.redirect(`${origin}/login?error=auth_callback_failed`);
