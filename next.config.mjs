be9a49801753fbb7793c22a7c0f4b07edcbb4ab4
CHARAN T M <charanntm.dev@gmail.com>
2026-07-30 20:56:25 +0530

    feat: updated remotePatterns and ready for prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: [
-    '10.147.190.104',
-    '10.46.35.104',
-    '10.19.224.104',
-    '10.69.71.104'
-  ],
-
   images: {
     remotePatterns: [
       {
@@ -24,6 +17,106 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'drive.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+
+      // Supabase Storage
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+      },
+
+      // Cloudinary
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+
+      // AWS S3
+      {
+        protocol: 'https',
+        hostname: '*.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.s3.amazonaws.com',
+      },
+
+      // DigitalOcean Spaces
+      {
+        protocol: 'https',
+        hostname: '*.digitaloceanspaces.com',
+      },
+
+      // Cloudflare Images / R2
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+      },
+
+      // Imgur
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+
+      // ImageKit
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+      },
+
+      // Firebase Storage
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+
+      // GitHub
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+
+      // Unsplash
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+
+      // Pexels
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+
+      // Pixabay
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+      },
+
+      // Freepik CDN
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+      },
+
+      // Wikimedia
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
       },
     ],
   },
